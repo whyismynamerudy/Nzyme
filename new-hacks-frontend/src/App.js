5252563e5e1a6ef0b3d011f71dd8fc60318c66ba
Usama Mohiuddin <usamamohiudin@gmail.com>
2022-11-05 20:38:43 -0400

    123

--- new-hacks-frontend/src/App.js
+++ new-hacks-frontend/src/App.js
@@ -1,20 +1,74 @@
-import React from 'react';
-import Nav from './components/Navbar';
-import Footer from './components/Footer';
-
-
-import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './logo.svg';
 import './App.css';
+
+
 
 
 function App() {
   return (
+    
     <div className="App">
-      <Nav />
-      <h1>Hello world</h1>
-      <Footer />
+      <h1>Summarizer App</h1>
+
+    <div>
+      <form className  = "Label1">
+        <label >
+        Text Input Box:
+        
+        <input type="text" name="name" className = "App-Form1"/>
+        <style>
+          background-color : #ffc;
+        </style>
+        </label >
+        <input type="submit" value="Submit" />
+      </form>
+
+      <form className  = "Label2">
+        <label   >
+        Summary Box:
+
+        <input type="text" name="name" className = "App-Form2" />
+        </label>
+        <style>
+        </style>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
-  );
+
+    
+
+
+
+
+
+      {/* find out out to use app*/}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        {/* this is used for importing the react symbol*/}
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+
+
+
+  );  
 }
 
 export default App;
+
+
+
+  
+
+
