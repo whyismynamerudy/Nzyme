83aa8a019e27aed8806b732aa175b80fa342e73b
Rudy <rudraksh.monga@mail.utoronto.ca>
2022-11-06 10:08:17 -0500

    current final version

--- new-hacks-frontend/src/components/Recall.jsx
+++ new-hacks-frontend/src/components/Recall.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
+import '../styles/Recall.css'
 
 const Recall = (props) => {
 
-  const words = props.subsetwords.map(word => <li>{word}</li>);
+  const words = props.subsetwords.map(word => <li className='keywordItem'>{word}</li>);
 
   if (!props.used){
     return <></>
   } else {
     return (
-      <div>
-        <h1>Active Recall</h1>
-        <p dangerouslySetInnerHTML={ {__html: props.summary} }></p>
-        <p>is this <b>bold</b></p>
-        <p>Keywords:</p>
-        <ul>
+      <>
+      <div className='recallContainer'>
+        <h1 className='recallHeader'>Active Recall</h1>
+        <p dangerouslySetInnerHTML={ {__html: props.summary} } className='summaryBold'></p>
+      </div>
+      <div className='keywordsContainer'>
+        <p className='keywordsHeader'><b>Keywords:</b></p>
+        <ul className='keywordsList'>
           {words}
         </ul>
       </div>
+    </>
     )
   }
 }
